fc3f46c33ba7c2cc08b8b6ee1b72ca5e813f8f2b
adam N <adam.nawroc@gmail.com>
2018-02-21 18:19:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(artist, tool) {
   arr = []
 
   for (var i=0; i<artist.length; i++) {
-    arr.push(`${artist} plays ${tool}`)
+    arr.push(`${artist[i]} plays ${tool[i]}`)
 }
 
 return arr
